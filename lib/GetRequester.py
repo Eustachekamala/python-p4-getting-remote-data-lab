50c443f7441b50026263008335fdae91d8d4cb10
eustachekamala <eustachejared25@gmail.com>
2024-10-06 22:20:47 +0300

    Update the Exemple usage

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -15,7 +15,7 @@ class GetRequester:
         response_body = self.get_response_body()
         return json.loads(response_body)
 
-# Example usage (to be done outside this file):
+# Example usage
 get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
 data = get_requester.load_json()
 print(data)

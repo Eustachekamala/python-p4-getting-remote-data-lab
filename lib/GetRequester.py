401c2813df50dcda72e3ffac0d18e8fa3572564d
eustachekamala <eustachejared25@gmail.com>
2024-10-06 22:19:52 +0300

    I created a class GetRequester that is a class for retrieving remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,20 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        """Send a GET request to the URL and return the response body as a JSON string."""
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        """Use get_response_body to get the response, then convert it to a Python object."""
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+
+# Example usage (to be done outside this file):
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+data = get_requester.load_json()
+print(data)
